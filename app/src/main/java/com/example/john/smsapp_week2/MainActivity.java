509317effff952dd33c661117d99844492411ca5
@joekifle <joey.kifle@gmail.com>
2019-11-05 21:25:54 +0300

    Add send sms method

--- app/src/main/java/com/example/john/smsapp_week2/MainActivity.java
+++ app/src/main/java/com/example/john/smsapp_week2/MainActivity.java
@@ -1,13 +1,57 @@
 package com.example.john.smsapp_week2;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.telephony.SmsManager;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+     EditText txtMobile;
+     EditText txtMessage;
+     Button btnSms;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        txtMobile = (EditText)findViewById(R.id.mblTxt);
+        txtMessage = (EditText)findViewById(R.id.msgTxt);
+        btnSms = (Button)findViewById(R.id.btnSend);
+
+    }
+
+    public void sendSMS(View view){
+
+
+        /*
+             try{
+                    SmsManager smgr = SmsManager.getDefault();
+                    smgr.sendTextMessage(txtMobile.getText().toString(),null,txtMessage.getText().toString(),null,null);
+                    Toast.makeText(MainActivity.this, "SMS Sent Successfully", Toast.LENGTH_SHORT).show();
+                }
+                catch (Exception e){
+                    Toast.makeText(MainActivity.this, "SMS Failed to Send, Please try again", Toast.LENGTH_SHORT).show();
+                }
+        */
+
+        try{
+            Intent i = new Intent(Intent.ACTION_VIEW);
+            i.setData(Uri.parse("smsto:"));
+            i.setType("vnd.android-dir/mms-sms");
+            i.putExtra("address", new String(txtMobile.getText().toString()));
+            i.putExtra("sms_body",txtMessage.getText().toString());
+            startActivity(Intent.createChooser(i, "Send sms via:"));
+        }
+        catch(Exception e){
+            Toast.makeText(MainActivity.this, "SMS Failed to Send, Please try again", Toast.LENGTH_SHORT).show();
+        }
+
     }
 }
